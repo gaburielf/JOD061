4afa67b19cbceb9859957d9ea068e305b25a1bc2
gaburielf <gaburielf@gmail.com>
2020-10-17 03:08:48 -0300

    Atividades 1 a 3

---
+++ Atividades/Atividade02/Cliente/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Cliente
+{
+    class Program
+    {
+        const int porta = 7000;
+        const string ip = "192.168.1.7";
+
+        static void Main(string[] args)
+        {
+            TcpClient client = new TcpClient();
+            Console.Write("Conectando ao servidor");
+            try
+            {
+                client.Connect(ip, porta);
+                Console.WriteLine("Ok");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Falhou");
+                return;
+            }
+
+            byte[] bytes = new byte[1024];
+            NetworkStream stream = client.GetStream();
+
+            Console.WriteLine("Envie uma mensagem. Pressione ENTER para sair.");
+            Console.Write("> ");
+            string mensagem = Console.ReadLine();
+
+            while (msg.Length > 0)
+            {
+                bytes = Encoding.ASCII.GetBytes(mensagem);
+                stream.Write(bytes, 0, bytes.Length);
+
+                Console.Write("> ");
+                mensagem = Console.ReadLine();
+            }
+
+            stream.Close();
+            Console.WriteLine("Desconectado");
+            client.Close();
+        }
+    }
+}
---
+++ Atividades/Atividade03/chat/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace JOD061
+{
+    class Program
+    {
+        const int porta_ = 7000;
+        const string ip = "192.168.1.7";
+        static void Main(string[] args)
+        {
+            try
+            {
+                IPEndPoint endP = new IPEndPoint(IPAddress.Any, 0);
+                UdpClient porta = new UdpClient(porta_);
+
+                Console.WriteLine("Para sair, aperte - CTRL + C");
+                while (true)
+                {
+                    byte[] bytesRecebidos = porta.Receive(ref endP);
+                    Console.WriteLine("Mensagem recebida: {0}", Encoding.ASCII.GetString(bytesRecebidos));
+                    Console.WriteLine("Mensagem enviada por {0}:{1}",endP.Address.ToString(), endP.Port.ToString());
+                    
+                }
+            }
+            catch (Exception)
+            {
+                UdpClient par = new UdpClient();
+                par.EnableBroadcast = true;
+
+                Console.WriteLine("Envie sua mensagem. Precione ENTER para sair.");
+                Console.WriteLine("> ");
+                string mensagem = Console.ReadLine();
+
+                while (mensagem.Length > 0)
+                {
+                    byte[] bytesEnviados = Encoding.ASCII.GetBytes(mensagem);
+                    par.Send(bytesEnviados, bytesEnviados.Length, ip, porta_);
+
+                    Console.Write("> ");
+                    mensagem = Console.ReadLine();
+
+                }
+                par.Close();
+
+            }
+        }
+    }
+}
